d32b0b5e816e90b7929913f75bfe97b05ae7f599
Franco Reyes <frankkog09@gmail.com>
2023-02-21 19:34:15 -0300

    master auto-commit

--- script.js
+++ script.js
@@ -10,3 +10,11 @@ document.write(sum(2, 2))
 let frutas = ["manzana", "banana", "durazno"]
 
 document.write(frutas[0])
+
+let persona = {
+    nombre: "Matias",
+    edad: 20,
+    dni: 1237483
+}
+
+document.write(persona["nombre"])
