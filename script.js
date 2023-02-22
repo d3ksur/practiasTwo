8e52410ba22acabfeef60d4cc99796de5cb67222
Franco Reyes <frankkog09@gmail.com>
2023-02-21 22:39:54 -0300

    master auto-commit

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ function sum(a, b){
     return a + b;
 }
   
-document.write(sum(2, 2))
+console.log(sum(2, 2))
 
 let frutas = ["manzana", "banana", "durazno"]
 
-document.write(frutas[0])
+console.log(frutas[0])
 
 let persona = {
     nombre: "Matias",
@@ -17,4 +17,34 @@ let persona = {
     dni: 1237483
 }
 
-document.write(persona["nombre"])
+console.log(persona["nombre"])
+
+// Creamos una función que retorna una promesa
+function obtenerUsuario(id) {
+  return new Promise((resolve, reject) => {
+    // Hacemos una petición a la API para obtener la información del usuario
+    fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+      .then(response => {
+        if (response.ok) {
+          // Si la respuesta es exitosa, resolvemos la promesa con los datos del usuario
+          resolve(response.json());
+        } else {
+          // Si hay un error en la respuesta, rechazamos la promesa con un mensaje de error
+          reject('Error al obtener usuario');
+        }
+      })
+      .catch(error => {
+        // Si hay un error en la petición, rechazamos la promesa con un mensaje de error
+        reject('Error en la petición');
+      });
+  });
+}
+
+// Llamamos a la función y manejamos la promesa con then y catch
+obtenerUsuario(1)
+  .then(usuario => {
+    console.log(usuario);
+  })
+  .catch(error => {
+    console.log(error);
+  });
